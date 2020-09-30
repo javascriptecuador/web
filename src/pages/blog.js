2a767588ff4b5193997d10c3702193fa7c8939b0
Edisson Reinozo <edisson350@gmail.com>
2020-09-29 21:33:58 -0500

    basic Blog setup

---
+++ src/pages/blog.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+import { StaticQuery, graphql, Link } from "gatsby";
+import { render } from "react-dom";
+
+function BlogPage(articulos) {
+  return (
+    <Layout>
+      <SEO title="Blog de Javascript Ecuador" />
+      <div>
+        <h2>Lista de blogs:</h2>
+
+        <ul>
+          {articulos.map((articulo) => (
+            <li>
+              <Link to={`/blog/${articulo.slug}`}>{articulo.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  );
+}
+
+function renderBlogPage() {
+  return (
+    <StaticQuery
+      query={graphql`
+        query BlogList {
+          articulos: allMdx {
+            edges {
+              node {
+                slug
+                frontmatter {
+                  title
+                  description
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => {
+        const articulosFormateados = data.articulos.edges.map((articulo) => {
+          return {
+            ...articulo.node.frontmatter,
+            slug: articulo.node.slug,
+          };
+        });
+        return BlogPage(articulosFormateados);
+      }}
+    />
+  );
+}
+
+export default renderBlogPage;
--- src/templates/blog-entry.js
+++ src/templates/blog-entry.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 // import PropTypes from 'prop-types';
 
-// export default function BlogEntry({pageContext, children}){
-    export default function BlogEntry(){
-    return  <div>
-                <h1>Blog entry</h1>
-                <hr />
-                {/* <p>{pageContext.title}</p>
-                {children} */}
-            </div>
+export default function BlogEntry({ pageContext, children }) {
+  return (
+    <Layout>
+      <SEO
+        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+        title="About"
+      />
+
+      <section className="flex flex-col md:flex-row">
+        <h1>{pageContext.frontmatter.title}</h1>
+        <section>{children}</section>
+        {/* <div className="md:w-2/3 md:mr-8"></div> */}
+      </section>
+    </Layout>
+  );
 }
 
 // BlogEntry.prototype = {

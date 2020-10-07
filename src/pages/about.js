1d1ad21daffa62ad648e18160371b54adaeee47a
Israel Teneda <israteneda@gmail.com>
2020-10-06 23:04:11 -0500

    fix: página about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import dogIllustration from "../images/dog-illustration.svg";
 
 function AboutPage() {
   return (

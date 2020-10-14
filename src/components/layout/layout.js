259df90118ef184681d58fae12cd943398677545
David <davidpadilla.f22@gmail.com>
2020-10-14 14:45:14 -0500

    fix: Diferenciar layout del home
    El layout del home es diferente de las demas paginas por lo que ahora
    con el prop home en layout se crea un layout diferente solo para el
    home.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,21 +1,43 @@
 import PropTypes from "prop-types";
 import React from "react";
-import "./layout.css";
 import Footer from "../footer/footer";
 import Header from "../header";
-import SEO from "../seo";
+import styles from "./layout.module.css";
+import pathImg from '../../images/background/path.svg';
+import BlogSection from '../blog-section/blog-section';
+import Subscribe from "../subscribe/subscribe";
+
+const Layout = ({ home, children }) => {
+  let background = ''
+  let blogSection = null
+
+  if (home) {
+    background = pathImg
+    blogSection = <BlogSection />
+  }
+
+  return (
+    <div
+      className={styles.layout}
+      style={{ backgroundImage: `url(${background})` }}>
+      <Header />
+      <main className={styles.mainContent}>{children}</main>
+      <Subscribe />
+      {blogSection}
+      <Footer />
+    </div>
+  );
+}
 
 
-const Layout = ({ children }) => (
-  <div className="layout flex flex-col min-h-screen font-sans text-gray-900">
-    <Header />
-    <main className="main-content">{children}</main>
-    <Footer />
-  </div>
-);
 
 Layout.propTypes = {
+  home: PropTypes.bool, // Indicates whether it is the homepage or not
   children: PropTypes.node.isRequired,
 };
+
+Layout.propTypes = {
+  home: false
+}
 
 export default Layout;

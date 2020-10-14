3935cd8fc94d147bd7de62dcd0c46b0e44edf5f8
David <davidpadilla.f22@gmail.com>
2020-10-13 22:32:37 -0500

    fix(home): Mejora de diseño responsive de cabecera
    La cabecera del home ahora se adapta de una mejor manera a dispositivos moviles

--- src/components/hero-section/hero-section.js
+++ src/components/hero-section/hero-section.js
@@ -6,15 +6,15 @@ import SocialNetworks from './social-networks/social-networks';
 import javascriptMapWidget from '../../images/widgets/js-ecuador-map.svg'
 
 const HeroSection = ({ title, subtitle }) => (
-  <div className="hero-section ec-container ">
+  <section className="hero-section ec-container ">
     <div className="hero-text">
       <h1 className="hero-title">{title}</h1>
       <h4 className="hero-subtitle">{subtitle}</h4>
       <CommunityDescription />
       <SocialNetworks />
-      <img className="widget map" src={javascriptMapWidget}  />
+      <img className="ec-map" src={javascriptMapWidget}  />
     </div>
-  </div>
+  </section>
 );
 
 HeroSection.propTypes = {

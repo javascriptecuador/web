10953bb6c81de2c5608c753538e9a8982eeff1bf
David <davidpadilla.f22@gmail.com>
2020-10-17 23:20:49 -0500

    feat: Crear seccion mantenedores
    Creacion de la seccion de mantenedores en la pagina de comunidad

--- src/pages/comunidad.js
+++ src/pages/comunidad.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Contributors from "../components/contributors/contributors";
 import Layout from "../components/layout/layout";
+import Maintainers from "../components/maintainers/maintainers";
 import SEO from "../components/seo";
 
 const ComunidadPage = () => (
@@ -9,6 +10,7 @@ const ComunidadPage = () => (
       keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
       title="Home"
     />
+    <Maintainers />
     <Contributors />
   </Layout>
 );

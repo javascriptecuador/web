83cbdcfa86898ac7f3d1ed2f6cc569e50032f826
Andres Alba <bryanalba92@gmail.com>
2020-10-21 23:25:49 -0500

    feat: aniadir seccion ¿Como ayudar?

---
+++ src/components/community-hero-section/card-section/card-section.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './card-section.css';
+import Button from '../../UI/button/button';
+import PropTypes from 'prop-types'
+
+const HeroSection = ({title, description, txtButton, descriptionExample}) => {
+    return (
+        <div className='card-section'>
+            <div className='section-description'>
+                <h5 className="topic-description">{title}</h5> 
+                <div className="description">{description}</div>
+                <Button type="primary">{txtButton}</Button>
+            </div> 
+            <div className='section-example'>
+                <h5 className="topic-example">Ejemplo</h5>
+                <div className="description-example">{descriptionExample}</div>
+            </div> 
+        </div>
+    )
+}
+
+HeroSection.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    txtButton: PropTypes.string,
+    descriptionExample: PropTypes.string,
+  }
+  
+
+export default HeroSection;

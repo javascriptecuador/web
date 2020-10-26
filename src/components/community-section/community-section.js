8afb455a4fb06694861b109d1e591684a0834b51
Richard Palacios G <apalaciosg91@gmail.com>
2020-10-26 14:20:20 -0500

    feat(slider): add photos for slider in community description

--- src/components/community-section/community-section.js
+++ src/components/community-section/community-section.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import Button from '../UI/button/button'
-import CommunityDescription from './community-description/community-description';
-import PhotoSlider from './photo-slider/photo-slider';
-import circleWidget from '../../images/widgets/circle.svg'
-import squareWidget from '../../images/widgets/square.svg'
-import './community-section.css'
+import React from "react";
+import Button from "../UI/button/button";
+import CommunityDescription from "./community-description/community-description";
+import PhotoSlider from "./photo-slider/photo-slider";
+import circleWidget from "../../images/widgets/circle.svg";
+import squareWidget from "../../images/widgets/square.svg";
+import photo1 from "../../images/slider/1era meetup cuenca.jpg";
+import photo2 from "../../images/slider/segunda meetup.jpg";
+import "./community-section.css";
 
 const CommunitySection = () => {
   const photos = [
     {
-      name: 'photo1',
-      photoURL:
-        'https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ',
+      name: "1era meetup",
+      photoURL: photo1,
     },
     {
-      name: 'photo2',
-      photoURL:
-        'https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ',
+      name: "2da meetup - Trabajo remoto mitos y realidades",
+      photoURL: photo2,
     },
     {
-      name: 'photo3',
+      name: "photo3",
       photoURL:
-        'https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68',
+        "https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
     },
     {
-      name: 'photo4',
+      name: "photo4",
       photoURL:
-        'https://i.picsum.photos/id/100/2500/1656.jpg?hmac=gWyN-7ZB32rkAjMhKXQgdHOIBRHyTSgzuOK6U0vXb1w',
+        "https://i.picsum.photos/id/100/2500/1656.jpg?hmac=gWyN-7ZB32rkAjMhKXQgdHOIBRHyTSgzuOK6U0vXb1w",
     },
     {
-      name: 'photo5',
+      name: "photo5",
       photoURL:
-        'https://i.picsum.photos/id/864/598/386.jpg?hmac=DOo0-DGwCVWdAnQAN_IHDZ2kKK_t10AGle49ztEbyYM',
+        "https://i.picsum.photos/id/864/598/386.jpg?hmac=DOo0-DGwCVWdAnQAN_IHDZ2kKK_t10AGle49ztEbyYM",
     },
   ];
   return (
@@ -50,15 +50,13 @@ const CommunitySection = () => {
         </ul>
         <CommunityDescription />
         <div className="call-to-action">
-          <Button type="secondary">
-            Conocer a la Comunidad
-          </Button>
+          <Button type="secondary">Conocer a la Comunidad</Button>
           <Button type="secondary">Cómo ayudar?</Button>
           <Button type="primary">Código de Conducta</Button>
         </div>
       </div>
-      <img className="widget circle" src={circleWidget}  />
-      <img className="widget square" src={squareWidget}  />
+      <img className="widget circle" src={circleWidget} />
+      <img className="widget square" src={squareWidget} />
     </section>
   );
 };

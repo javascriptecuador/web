e3bf7840a740973dee4e31075bb247a3e2738f6c
David <davidpadilla.f22@gmail.com>
2020-10-27 17:11:04 -0500

    fix: mejora de estilos y animaciones para navbar
    - Uso de css modules y resaltado del navlink activo
    - Animaciones para navbar

---
+++ src/components/header/Logo/logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import escudo from "../../../images/logo-escudo-ec.svg";
+import styles from './logo.module.css';
+
+const Logo = () => (
+  <div className={styles.logo}>
+    <h1>
+      <span className={styles.shield}>
+        <img src={escudo} alt="Logo de Javascript Ecuador" />
+      </span>
+      <span className={styles.legend}>Ecuador.js</span>
+    </h1>
+  </div>
+);
+
+export default Logo;

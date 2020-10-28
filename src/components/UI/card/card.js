6133de1a57bdf8f217f6e94feeac76af4650f555
David <davidpadilla.f22@gmail.com>
2020-10-27 20:15:27 -0500

    fix: arreglo de estilos en la seccion eventos

--- src/components/UI/card/card.js
+++ src/components/UI/card/card.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import './card.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./card.module.css";
 import { Link } from "gatsby";
 
-
-const Card = ({ imgURL, topic, date, descriptionTitle, description, link, readMoreColor, topicFontSize }) => (
-  <div className="card">
-    <div className="card-header">
-      <div className="img-viewport">
+const Card = ({
+  imgURL,
+  topic,
+  dateTime,
+  descriptionTitle,
+  description,
+  link,
+  readMoreColor,
+  topicFontSize,
+  viewportHeight,
+  textSpacing,
+  lineHeight,
+}) => (
+  <div className={styles.card}>
+    <div className={styles.card__header}>
+      <div className={styles.card__imgViewport} style={{ height: viewportHeight }}>
         <img src={imgURL} />
       </div>
     </div>
-    <div className="card-body">
-      <h5 className="topic" style={{fontSize: topicFontSize}}>{topic}</h5>
-      <p className="date">{date}</p>
-      <div className="description">
-        <p className="description-text">
-          <strong className="description-title">{descriptionTitle}:</strong>
-          {description} <Link to={link} className="read-more" style={{color: readMoreColor}}>Leer más</Link>
+    <div className={styles.card__body}>
+      <h5
+        className={styles.card__topic}
+        style={{
+          fontSize: topicFontSize,
+          marginBottom: textSpacing,
+          lineHeight,
+        }}>
+        {topic}
+      </h5>
+      <p className={styles.card__date} style={{ lineHeight }}>
+        {dateTime}
+      </p>
+      <div className={styles.card__description} style={{ marginTop: textSpacing }}>
+        <p className={styles.card__descriptionText}>
+          <strong className={styles.card__descriptionTitle}>{descriptionTitle}:</strong>
+          {description}{" "}
+          <Link
+            to={link}
+            className={styles.card_readmore}
+            style={{ color: readMoreColor }}>
+            Leer más
+          </Link>
         </p>
       </div>
     </div>
@@ -27,18 +54,24 @@ const Card = ({ imgURL, topic, date, descriptionTitle, description, link, readMo
 Card.propTypes = {
   imgURL: PropTypes.string,
   topic: PropTypes.string,
-  date: PropTypes.string,
+  dateTime: PropTypes.string,
   descriptionTitle: PropTypes.string,
   description: PropTypes.string,
   link: PropTypes.string,
   readMoreColor: PropTypes.string,
-}
+  viewportHeight: PropTypes.string,
+  textSpacing: PropTypes.string,
+  lineHeight: PropTypes.string,
+};
 
 Card.defaultProps = {
   date: null,
-  descriptionTitle: 'Descripción',
+  descriptionTitle: "Descripción",
   readMoreColor: "#27ae60",
-  topicFontSize: "2rem"
-}
+  topicFontSize: "2rem",
+  viewportHeight: "136.13px",
+  textSpacing: "0.5em",
+  lineHeight: "auto",
+};
 
 export default Card;

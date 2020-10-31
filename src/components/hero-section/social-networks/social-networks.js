5bc6363a33256ad0885526eea1ab213c78d186f3
jordandev <rjaramilloby@gmail.com>
2020-10-30 21:09:34 -0500

    feat(social-networks.js): corrigiendo rutas de redireccionamiento

--- src/components/hero-section/social-networks/social-networks.js
+++ src/components/hero-section/social-networks/social-networks.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import githubSVG from '../../../images/icons/github.svg';
-import telegramSVG from '../../../images/icons/telegram.svg';
-import twitterSVG from '../../../images/icons/twitter.svg';
-import eventbriteSVG from '../../../images/icons/eventbrite.png';
-import './social-networks.css'
+import React from "react";
+import githubSVG from "../../../images/icons/github.svg";
+import telegramSVG from "../../../images/icons/telegram.svg";
+import twitterSVG from "../../../images/icons/twitter.svg";
+import eventbriteSVG from "../../../images/icons/eventbrite.png";
+import "./social-networks.css";
 
 const SocialNetworks = () => (
   <div className="social-networks">
@@ -12,14 +12,17 @@ const SocialNetworks = () => (
       <a href="https://github.com/javascriptecuador/web" target="_blank">
         <img src={githubSVG} alt="imagen github" />
       </a>
-      <a href="#">
-        <img src={telegramSVG} alt="imagen github" />
+      <a href="https://t.me/javascriptecuador" target="_blank">
+        <img src={telegramSVG} alt="imagen telegram" />
       </a>
-      <a href="#">
-        <img src={twitterSVG} alt="imagen github" />
+      <a href="https://twitter.com/jsecuador_" target="_blank">
+        <img src={twitterSVG} alt="imagen twitter" />
       </a>
-      <a href="#">
-        <img src={eventbriteSVG} alt="imagen github" />
+      <a
+        href="https://www.eventbrite.com/o/javascript-ecuador-29109297643?aff=eand"
+        target="_blank"
+      >
+        <img src={eventbriteSVG} alt="imagen eventBrite" />
       </a>
     </div>
   </div>

0f0e6ee1d6b41f64ece03fb5ffcaa7922e01c423
Karlos <admin@karlosencalada.dev>
2020-10-31 05:05:29 -0500

    feat(footer.js): se agrega footer responsive

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "gatsby";
+import styles from './footer.module.css';
+import escudo from "../../images/logo-escudo-ec.svg";
+//import SocialNetworks from './social-networks';
 
 const Footer = () => (
-  <footer className="bg-yellow-500">
-    <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
-      <p className="text-white font-bold">JavaScript Ecuador</p>
+  <footer className={styles.footer}>
+    <div className={styles.flex}>
+      <div className={styles.col}>
+        <h4 className={styles.footer__title}>RECURSOS</h4>
+        <ul>
+          <li>Acerca de</li>
+          <Link to="/blog" ><li>Blog</li> </Link>
+          <Link to="/cuentas"><li>Cuentas</li></Link>
+        </ul>
+      </div>
+      <div className={styles.col}>
+        <h4 className={styles.footer__title}>AYUDA</h4> 
+        <ul>
+          <li>Organizar un Meetup</li>
+          <li>Dar una Charla/Taller</li>
+          <li>Publicar un Post</li>
+          <li>Informar un problema</li>
+          <Link to="/code-of-conduct"><li>Código de Conducta</li></Link>
+        </ul>
+      </div>
 
-      <p>
-        <a
-          className="font-bold text-white no-underline"
-          href="https://github.com/javascriptecuador/web"
-          target="_blank"
-          rel="noopener noreferrer">
-          GitHub
-        </a>
-      </p>
-    </nav>
+      <div className={styles.col}>
+        <h4 className={styles.footer__title}>COMUNIDAD</h4>
+        <ul>
+        <Link to="/eventos"><li>Eventos</li></Link>
+          <li>¿Cómo ayudar?</li>
+          <li>Quiero ser Patrocinador</li>
+          <li>Registrar mi ciudad</li>
+          <li>Descargo de responsabilidad</li>
+        </ul>
+      </div>
+
+      <div className={styles.col}>
+        <h1 >Ecuador.js</h1>
+        <img src={escudo} alt="Escudo del Ecuador" />
+        {/* <SocialNetworks /> */}
+      </div>
+    </div>
+
+    <p>¿Encontraste un error? <span><a href="https://github.com/javascriptecuador/web/issues/new/choose"  target="_blank">Repórtalo </a></span></p>
+    
+    <h2>2019 - 2020 Ecuador.js</h2>
+    <h3>All rights reserved</h3>
+
   </footer>
 );
 
